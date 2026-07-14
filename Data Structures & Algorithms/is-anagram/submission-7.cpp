fd9d20a3212f07e4e5184db8f5971c44170b9fa0
Abhishek M <abhishekakshu1512@gmail.com>
2026-07-14 21:52:16 +0530

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.cpp
@@ -0,0 +1,90 @@
+// Optimal 
+
+
+// SET :
+
+// class Solution {
+// public:
+//     bool hasDuplicate(vector<int>& nums) {
+        
+//         int n = nums.size();
+//         unordered_set<int>st;
+
+//         for (int x : nums){
+//             if(st.find(x) != st.end()){
+//                 return true;
+//             }
+//             st.insert(x);
+
+//             }
+//         return false;
+//     }
+// };
+
+
+//===============
+
+// MAP:
+
+
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        
+        int n = nums.size();
+        unordered_map<int,int>mp;
+
+        for (int i=0; i<n; i++){
+            mp[nums[i]]++;
+            if(mp[nums[i]] > 1) {
+                return true;
+        }        }
+        return false;
+    }
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// Brutte Force Solution
+   //  Time Complexity = O(n²)  &&  Space Complexity = O(1)
+
+// class Solution {
+// public:
+//     bool hasDuplicate(vector<int>& nums) {
+        
+//         int n = nums.size();
+//         for(int i=0; i<n; i++){
+//             for(int j=i+1; j<n; j++){
+//                 if(nums[i]==nums[j])
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+//=================================================
+
+// Overall:
+
+// Time: O(n log n)
+// Space: Depends on the sorting implementation. For std::sort, it's typically O(log n) due to recursion stack.
+
+// #include<bits/stdc++.h>
+// class Solution {
+// public:
+//     bool hasDuplicate(vector<int>& nums) {
+        
+//         int n = nums.size();
+//         sort(nums.begin(), nums.end() );
+//            for(int i=0; i<n-1; i++){
+//                 if(nums[i]==nums[i+1]){
+//                 return true;
+//                 }
+//         }
+//         return false;
+//     }
+// };
+
+
+//======================================================================================================

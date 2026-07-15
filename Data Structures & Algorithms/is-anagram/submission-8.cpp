60249f29dae997d11c51d06a1c5f3ee7dc7931f2
Abhishek M <abhishekakshu1512@gmail.com>
2026-07-15 07:39:26 +0530

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.cpp
@@ -0,0 +1,79 @@
+
+
+//Brutte Solution
+   // TC = O(N logN)    
+   // SC = O(1) => (If sorting is considered in-place; otherwise it depends on the sort implementation.)
+
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+        
+//         if(s.length() != t.length() ){
+//             return false;
+//         }
+
+//         sort(s.begin(), s.end());
+//         sort(t.begin(), t.end());
+
+//         return s == t;
+//     }
+// };
+
+//======================================================================================================
+                     // Better Solution 
+//  TC =O(n)
+// SC = (k) where k is the number of distinct characters (at most 26 for lowercase English letters).
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+        
+//         unordered_map<char,int>mp;
+//         if(s.length() != t.length() ){
+//             return false;
+//         }
+
+//         for(auto x:s){
+//             mp[x]++;
+//         }
+//         for (auto ch:t){
+//             mp[ch]--;
+//         }
+
+//          for (auto p : mp)
+//         {
+//             if (p.second != 0)
+//                 return false;
+//         }
+//         return true;
+
+//     }
+// };
+
+//========================================================================
+
+// Optimal Solutions:
+
+class Solution {
+    public:
+     bool isAnagram(string s, string t) {
+
+         if(s.length() != t.length() ){
+             return false;
+         }
+
+         int arr[26] = {0};
+
+         for(char ch:s)
+            arr[ch-'a']++;
+
+        for(auto ch:t)
+            arr[ch-'a']--;
+
+        for(int n:arr){
+            if(n!=0){
+                return false;
+            }
+        }
+        return true;
+     }
+};

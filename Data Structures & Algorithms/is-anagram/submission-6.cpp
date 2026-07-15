754e0fb2d5100191e8d1419cdd794096d46cf3a2
Abhishek M <abhishekakshu1512@gmail.com>
2026-07-15 07:33:30 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.cpp
@@ -0,0 +1,50 @@
+
+
+//Brutte Solution
+   // TC = O(N logN)    
+   // SC = O(1) => (If sorting is considered in-place; otherwise it depends on the sort implementation.)
+
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+        
+//         if(s.length() != t.length() ){
+//             return false;
+//         }
+
+//         sort(s.begin(), s.end());
+//         sort(t.begin(), t.end());
+
+//         return s == t;
+//     }
+// };
+
+//======================================================================================================
+                     // Better Solution 
+//  TC =O(n)
+// SC = (k) where k is the number of distinct characters (at most 26 for lowercase English letters).
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        unordered_map<char,int>mp;
+        if(s.length() != t.length() ){
+            return false;
+        }
+
+        for(auto x:s){
+            mp[x]++;
+        }
+        for (auto ch:t){
+            mp[ch]--;
+        }
+
+         for (auto p : mp)
+        {
+            if (p.second != 0)
+                return false;
+        }
+        return true;
+
+    }
+};

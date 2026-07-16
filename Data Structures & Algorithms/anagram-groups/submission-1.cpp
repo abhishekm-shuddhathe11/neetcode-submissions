dbd3173d8e12b850b052bd632002a78a32746245
Abhishek M <abhishekakshu1512@gmail.com>
2026-07-17 00:31:44 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+
+        int n = strs.size();
+
+        vector<vector<string>> ans;
+        vector<bool> visited(n, false);
+
+        for (int i = 0; i < n; i++) {
+
+            if (visited[i])      continue;
+
+            vector<string> group;
+            group.push_back(strs[i]);
+            visited[i] = true;
+
+            for (int j = i + 1; j < n; j++) {
+
+                if (visited[j])
+                    continue;
+
+                if (strs[i].length() != strs[j].length())
+                    continue;
+
+                int freq[26] = {0};
+
+                // Compare character frequencies
+                for (int k = 0; k < strs[i].length(); k++) {
+                    freq[strs[i][k] - 'a']++;
+                    freq[strs[j][k] - 'a']--;
+                }
+
+                bool isAnagram = true;
+
+                // Verify all frequencies became zero
+                for (int l = 0; l < 26; l++) {
+                    if (freq[l] != 0) {
+                        isAnagram = false;
+                        break;
+                    }
+                }
+
+                if (isAnagram) {
+                    group.push_back(strs[j]);
+                    visited[j] = true;
+                }
+            }
+
+            ans.push_back(group);
+        }
+
+        return ans;
+    }
+};

518a9ac650a040511aa272d31154a43a795c4fd4
Abhishek M <abhishekakshu1512@gmail.com>
2026-07-22 22:19:39 +0530

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.cpp
@@ -0,0 +1,72 @@
+// class Solution {
+// public:
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+
+//         unordered_map<int, int> mp;
+
+//         for (auto n : nums) {
+//             mp[n]++;
+//         }
+
+//         vector<pair<int, int>> arr;
+
+//         for (auto &p : mp) {
+//             arr.push_back({p.first, p.second});
+//         }
+
+//         sort(arr.begin(), arr.end(),
+//              [](const pair<int,int>& a, const pair<int,int>& b) {
+//                  return a.second > b.second;
+//              });
+
+//         vector<int> result;
+
+//         for (int i = 0; i < k && i < arr.size(); i++) {
+//             result.push_back(arr[i].first);
+//         }
+
+//         return result;
+//     }
+// };
+
+//==============================//
+
+//==============================//
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+
+        // Step 1: Count frequency of each element
+        unordered_map<int, int> freqMap;
+
+        for (int n : nums) {
+            freqMap[n]++;
+        }
+
+        // Step 2: Create buckets // Index = Frequency   // Value = List of elements having that frequency
+        vector<vector<int>> buckets(nums.size() + 1);
+
+        for (auto &[element, frequency] : freqMap) {
+            buckets[frequency].push_back(element);
+        }
+
+        // Step 3: Traverse buckets from highest frequency to lowest
+        vector<int> ans;
+
+        for (int freq = buckets.size() - 1; freq >= 1; freq--) {
+
+            for (int element : buckets[freq]) {
+
+                ans.push_back(element);
+
+                // Stop once we have k elements
+                if (ans.size() == k) {
+                    return ans;
+                }
+            }
+        }
+
+        return ans;
+    }
+};

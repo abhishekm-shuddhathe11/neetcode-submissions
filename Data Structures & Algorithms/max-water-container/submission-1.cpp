1eccead1761369dcab14550d7c61f1a3119e4e82
Abhishek M <abhishekakshu1512@gmail.com>
2026-08-04 23:14:45 +0530

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.cpp
@@ -0,0 +1,51 @@
+// Brutte Approach  TC = O(N^2)  SC = O(1)
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int largeArea = 0;
+
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                //Instead of this:
+                // int length = min(height[i] , height[j]);
+                // int breadth = j-i;
+                // int area =  length * breadth;
+
+                // Use this:
+                int area = min(height[i], height[j]) * (j - i);
+
+                largeArea = max(area, largeArea);
+            }
+        }
+        return largeArea;
+    }
+};
+
+// =========================================================================================================//
+
+// Optimal 2-pointer:
+
+// class Solution{
+// public:
+// int maxArea(vector<int>& height){
+//     int s=0 , e=height.size()-1;
+//     int LargeArea = 0;
+
+//     while(s<e){
+
+//         int area = min(height[s],height[e]) * (e-s);  
+//         LargeArea = max(area, LargeArea);
+
+//             if( height[s] <= height[e] ){
+//                 s++;
+//             }
+//             else
+//                 e--;
+
+//         //height[s] <= height[e] ? s++ : e-- ;   // ternary
+//       }
+//       return LargeArea;
+//     }
+// };

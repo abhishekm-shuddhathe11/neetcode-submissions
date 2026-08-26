36fde945b9bd39bb0cae9ef980abe6934681a006
Abhishek M <abhishekakshu1512@gmail.com>
2026-08-26 19:19:18 +0530

    Add: trapping-rain-water - submission-6

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-6.cpp
@@ -0,0 +1,63 @@
+// Brutte For(i){ for(j) }  & then apply formula
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+
+        int totalWater = 0;
+        int n = height.size();
+
+        for(int i=0; i<n; i++){    // i->n   ( 0 ->  <=i)  && ( =i -> n)
+ 
+            int LeftMax = 0, RightMax = 0;
+
+            for(int j =0; j<=i; j++){
+                LeftMax = max(LeftMax , height[j]);
+            }
+
+            for(int j=i; j<n; j++){
+                RightMax = max(RightMax , height[j]);
+            }
+                totalWater += min( LeftMax, RightMax ) - height[i];
+            
+        }
+         
+        return totalWater;
+    }
+};
+
+
+
+// Using Prefix max & Suffix Max arrays & using them in one final for loop
+
+// Optimal Solution ( TWO - POINTER) TC = O(N)   SC = O(1)
+
+// class Solution {
+// public:
+//     int trap(vector<int>& height) {
+
+//         int left =0 , right = height.size()-1;
+//         int leftMax = 0 , rightMax = 0;
+//         int totalWater = 0;
+
+//         while(left < right) {
+             
+//             if(height[left] < height[right]){
+//                 leftMax = max(leftMax, height[left]);
+//                 if( height[left]< leftMax){
+//                     totalWater += leftMax - height[left];
+//                 }
+//                 left++;
+//             }
+
+//             else{
+//                 rightMax = max(rightMax , height[right]); 
+//                 if(height[right]< rightMax){
+//                     totalWater += rightMax - height[right];
+//                 }
+//                 right--;
+//             }
+//         }
+//         return totalWater;
+//     }
+// };

0fd05ccaa03601327bdbea8ef05d9b453202b206
Abhishek M <abhishekakshu1512@gmail.com>
2026-09-07 22:17:55 +0530

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        stack<char>st;
+
+        if(s.empty()){
+            return true;
+        }
+
+        for( auto ch:s){
+
+            if( ch == '('|| ch== '[' || ch== '{'  ){
+                st.push(ch);
+            }
+            else{ 
+                if(!st.empty() ){
+                    char top = st.top();
+
+                    if( (top == '(' && ch ==')') || (top == '{' && ch =='}') || ( top == '[' && ch ==']') ){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+       return st.empty() ?  true :  false;
+    }
+};
